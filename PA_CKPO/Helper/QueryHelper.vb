18c4fe284b84497f4c020dbab18d44722f4554ea
ananta <anantayusra01@gmail.com>
2023-05-24 01:15:55 +0800

    update ListItemForm

--- PA_CKPO/Helper/QueryHelper.vb
+++ PA_CKPO/Helper/QueryHelper.vb
@@ -225,4 +225,25 @@
         Dim Datatable As DataTable = SqlHelper.ExecuteQuery(Query)
         Return Datatable
     End Function
+
+    Public Function ItemList(Optional Keyword As String = Nothing) As DataTable
+        Dim Query As String = $"SELECT
+            items.id,
+            items.code,
+            items.name,
+            REPLACE(FORMAT(CAST(items.price AS DECIMAL), 0), ',', '.') as price,
+            items.category,
+            items.color,
+            items.brand,
+            items.uom,
+            suppliers.name as supplier_name
+        FROM items
+        JOIN suppliers ON items.supplier_id=suppliers.id"
+        If Keyword IsNot Nothing Then
+            Query &= $" WHERE items.name LIKE '%{Keyword}%'"
+        End If
+        Query &= $" ORDER BY id DESC"
+        Dim Datatable As DataTable = SqlHelper.ExecuteQuery(Query)
+        Return Datatable
+    End Function
 End Module
