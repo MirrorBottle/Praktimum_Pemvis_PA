baab114e69f856645d1ef3d41e30dabeb60b9f31
DimasYudha1223 <dimasabdiyudha45@gmail.com>
2023-05-24 01:20:11 +0800

    CustomerLagi

--- PA_CKPO/Helper/QueryHelper.vb
+++ PA_CKPO/Helper/QueryHelper.vb
@@ -236,7 +236,6 @@
         Return Datatable
     End Function
 
-<<<<<<< HEAD
     Public Function ItemList(Optional Keyword As String = Nothing) As DataTable
         Dim Query As String = $"SELECT
             items.id,
@@ -252,7 +251,12 @@
         JOIN suppliers ON items.supplier_id=suppliers.id"
         If Keyword IsNot Nothing Then
             Query &= $" WHERE items.name LIKE '%{Keyword}%'"
-=======
+        End If
+        Query &= $" ORDER BY id DESC"
+        Dim Datatable As DataTable = SqlHelper.ExecuteQuery(Query)
+        Return Datatable
+    End Function
+
     Public Function CustomerList(Optional Keyword As String = Nothing) As DataTable
         Dim Query As String = $"SELECT
                 customers.id,
@@ -263,7 +267,6 @@
             FROM customers"
         If Keyword IsNot Nothing Then
             Query &= $" WHERE name LIKE '%{Keyword}%'"
->>>>>>> fe24010290914eaef6d36e1db87594e66b66ea9e
         End If
         Query &= $" ORDER BY id DESC"
         Dim Datatable As DataTable = SqlHelper.ExecuteQuery(Query)
